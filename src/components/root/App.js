1fac20997a069dcc967e56b8df543db3ca36f876
esmahaber <l1621012063@stud.sdu.edu.tr>
2020-10-31 19:12:02 +0300

    Navbar and Dashboard

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,8 +1,15 @@
+import Navi from "../navi/Navi";
+import Dashboard from "./Dashboard";
+import { Container } from "reactstrap";
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Container>
+        <Navi />
+        <Dashboard />
+      </Container>
     </div>
   );
 }
