7f1e8ad43b0fadebc5662f0fbdcf219e7155f6e0
esmahaber <l1621012063@stud.sdu.edu.tr>
2020-11-13 14:21:01 +0300

    CartSummary component

---
+++ src/components/cart/CartSummary.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavItem,
+  NavLink,
+  Badge,
+} from "reactstrap";
+
+class CartSummary extends Component {
+  emptyCart() {
+    return (
+      <NavItem>
+        <NavLink>Sepetiniz Boş</NavLink>
+      </NavItem>
+    );
+  }
+
+  renderSummary() {
+    return (
+      <UncontrolledDropdown nav inNavbar>
+        <DropdownToggle nav caret>
+          Sepet
+        </DropdownToggle>
+        <DropdownMenu right>
+          {this.props.cart.map((cartItem) => (
+            <DropdownItem key={cartItem.product.id}>
+              {cartItem.product.productName}
+              <Badge color="warning">{cartItem.quantity}</Badge>
+            </DropdownItem>
+          ))}
+          <DropdownItem divider />
+          <DropdownItem>Sepete Git</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.cart.length > 0 ? this.renderSummary() : this.emptyCart()}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    cart: state.cartReducer,
+  };
+}
+
+export default connect(mapStateToProps)(CartSummary);
